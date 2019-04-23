3e95f6346981de333da687a45909b3369e4f2df4
zed <manoa15@yahoo.com>
2019-04-24 02:08:26 +0300

    Adding underscore module to fix Allow origin access problem

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 var cors = require('cors');
+var _ = require('underscore');
 const {
     ObjectID
 } = require("mongodb");
@@ -56,13 +57,46 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-  }));
+
+function allowCrossDomain(req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+    var origin = req.headers.origin;
+    if (_.contains(app.get('allowed_origins'), origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    } else {
+        next();
+    }
+}
+
+app.configure(function () {
+    app.use(express.logger());
+    app.use(express.bodyParser());
+    app.use(allowCrossDomain);
+});
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://afternoon-atoll-25236.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use("/profile/cars", carRoutes); //Using carRoutes middleware( middleware - > code that runs between request and response)
 app.use("/auth/users", userRoutes);

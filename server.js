48f688b684d61529a54c8685102486e2741cff18
zed <manoa15@yahoo.com>
2019-04-24 01:38:40 +0300

    Adding middleware to make Access-control-allow-origin

--- server.js
+++ server.js
@@ -34,6 +34,25 @@ app.use(
 );
 app.use(bodyParser.json()); //to get the body of the request
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://afternoon-atoll-25236.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use("/profile/cars", carRoutes); //Using carRoutes middleware( middleware - > code that runs between request and response)
 app.use("/auth/users", userRoutes);
 app.use("/profile", profileRoutes);

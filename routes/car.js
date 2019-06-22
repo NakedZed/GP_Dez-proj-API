72e38ed6b39d1a70eff36c3bf596c979f6e96a01
zed <manoa15@yahoo.com>
2019-06-23 00:25:30 +0300

    requiring bodyParser in car.js file

--- routes/car.js
+++ routes/car.js
@@ -1,5 +1,6 @@
 //this is cars route file.
 const express = require("express");
+const bodyParser = require('body-parser')
 const router = express.Router(); //router object for routing;
 const {
   ObjectID
